7bd7c199e7073804715c84e2823a97305e8e1a46
William Smith <bill@talkingmoose.net>
2015-11-15 22:02:18 -0600

    Addressing issues
    Removed "success" dialog at end of script. Determined this is redundant
    if the password dialog appears.
    
    Added comments to all component files to associate them with Outlook
    Exchange Setup 5.0.
    
    Added option to prepend a NetBIOS domain name to the beginning of the
    user name.

--- ROOT/Library/Talking Moose Industries/Scripts/OutlookExchangeSetupLaunchAgent.sh
+++ ROOT/Library/Talking Moose Industries/Scripts/OutlookExchangeSetupLaunchAgent.sh
@@ -1,14 +1,37 @@
 #!/bin/sh
 
+# --------------------------------------------
+# Outlook Exchange Setup 5.0
+# © Copyright 2008-2015 William Smith
+# bill@officeformachelp.com
+# 
+# Except where otherwise noted, this work is licensed under
+# http://creativecommons.org/licenses/by/3.0/
+#
+# This file is one of four files for assisting a user with configuring
+# an Exchange account in Microsoft Outlook 2016 for Mac:
+# 
+# 1. Outlook Exchange Setup 5.0.scpt
+# 2. OutlookExchangeSetupLaunchAgent.sh
+# 3. net.talkingmoose.OutlookExchangeSetupLaunchAgent.plist
+# 4. com.microsoft.Outlook.plist for creating a configuraiton profile
+# 
+# These scripts and files may be freely modified for personal or commercial
+# purposes but may not be republished for profit without prior consent.
+# 
+# If you find these resources useful or have ideas for improving them,
+# please let me know. It is only compatible with Outlook 2016 for Mac.
+# --------------------------------------------
+
 # Check for the existence of the UBF8T346G9.Office folder.
 # If it doesn't exist then no Office 2016 for Mac application has run.
 # Create the folder create the OutlookProfile.plist file.
 # Also create a user LaunchAgents folder and an Outlook setup launchd agent.
 
 if [[ ! -d "$HOME/Library/Group Containers/UBF8T346G9.Office" ]] ; then
-	mkdir -p "$HOME/Library/Group Containers/UBF8T346G9.Office"
-	touch "$HOME/Library/Group Containers/UBF8T346G9.Office/OutlookProfile.plist"
-	mkdir -p "$HOME/Library/LaunchAgents"
+	/bin/mkdir -p "$HOME/Library/Group Containers/UBF8T346G9.Office"
+	/usr/bin/touch "$HOME/Library/Group Containers/UBF8T346G9.Office/OutlookProfile.plist"
+	/bin/mkdir -p "$HOME/Library/LaunchAgents"
 
 	launchagent='<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -38,9 +61,9 @@ if [[ ! -d "$HOME/Library/Group Containers/UBF8T346G9.Office" ]] ; then
 </plist>
 '
 
-	echo "$launchagent" >> "$HOME/Library/LaunchAgents/net.talkingmoose.OutlookExchangeSetup5.0.plist"
-	chmod 644 "$HOME/Library/LaunchAgents/net.talkingmoose.OutlookExchangeSetup5.0.plist"
-	launchctl load "$HOME/Library/LaunchAgents/net.talkingmoose.OutlookExchangeSetup5.0.plist"
+	/bin/echo "$launchagent" >> "$HOME/Library/LaunchAgents/net.talkingmoose.OutlookExchangeSetup5.0.plist"
+	/bin/chmod 644 "$HOME/Library/LaunchAgents/net.talkingmoose.OutlookExchangeSetup5.0.plist"
+	/bin/launchctl load "$HOME/Library/LaunchAgents/net.talkingmoose.OutlookExchangeSetup5.0.plist"
 	
 fi
 
@@ -48,11 +71,11 @@ fi
 # If anything exists, an Exchange account exists. Nothing more to do.
 # Unload and remove the launch agent if it stil exists.
 
-profile=$( defaults read "$HOME/Library/Group Containers/UBF8T346G9.Office/OutlookProfile.plist" Default_Profile_Name )
+profile=$( /usr/bin/defaults read "$HOME/Library/Group Containers/UBF8T346G9.Office/OutlookProfile.plist" Default_Profile_Name )
 
 if [[ -f "$HOME/Library/Group Containers/UBF8T346G9.Office/Outlook/Outlook 15 Profiles/$profile/Data/Exchange Accounts/"* ]] ; then
-	launchctl unload "$HOME/Library/LaunchAgents/net.talkingmoose.OutlookExchangeSetup5.0.plist"
-	rm "$HOME/Library/LaunchAgents/net.talkingmoose.OutlookExchangeSetup5.0.plist"
+	/bin/launchctl unload "$HOME/Library/LaunchAgents/net.talkingmoose.OutlookExchangeSetup5.0.plist"
+	/bin/rm "$HOME/Library/LaunchAgents/net.talkingmoose.OutlookExchangeSetup5.0.plist"
 fi
 
 exit 0
